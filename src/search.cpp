bf69901d477dd5a4027dee8a56f03cc2b61d01fb
KazApps <kazuki_shogi@outlook.jp>
2025-08-01 15:54:11 +0900

    bench: 4942877

--- src/search.cpp
+++ src/search.cpp
@@ -795,6 +795,7 @@ namespace stoat {
                           .empty();
                 r += !improving;
                 r -= history / 8192;
+                r += expectedCutnode * 3;
 
                 const auto reduced = std::min(std::max(newDepth - r, 1), newDepth - 1) + kPvNode;
                 curr.reduction = newDepth - reduced;
