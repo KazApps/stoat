7075cfac71d9fb35a788b56d2a07432ba202b734
KazApps <kazuki_shogi@outlook.jp>
2025-08-02 09:33:13 +0900

    bench: 4962967

--- src/search.cpp
+++ src/search.cpp
@@ -651,7 +651,7 @@ namespace stoat {
                 }
             }
 
-            if (depth >= 4 && curr.staticEval >= beta + 70 && !parent->move.isNull()) {
+            if (depth >= 4 && curr.staticEval >= beta + complexity / 5 && !parent->move.isNull()) {
                 const auto r = 3 + depth / 5;
 
                 const auto [newPos, guard] = thread.applyNullMove(ply, pos);
