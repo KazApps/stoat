bf691556f5bf3d2590e6a539814da7569d6387e9
KazApps <kazuki_shogi@outlook.jp>
2025-08-03 19:13:54 +0900

    bench: 4936266

--- src/eval/eval.cpp
+++ src/eval/eval.cpp
@@ -39,8 +39,7 @@ namespace stoat::eval {
         const i32 depth
     ) {
         const auto eval = staticEval(pos, nnueState);
-        const Score scaledEval = eval * (1024 + ply) / 1024;
         const auto correction = corrhist.correction(pos);
-        return std::clamp(scaledEval + correction * 16384 / (16384 + (32 - depth) * 16), -kScoreWin + 1, kScoreWin - 1);
+        return std::clamp(eval + correction * 16384 / (16512 - (ply + depth) * 16), -kScoreWin + 1, kScoreWin - 1);
     }
 } // namespace stoat::eval
