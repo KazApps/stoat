b9d6df22752458a9a8e161059bcf061ffa27ffaf
KazApps <kazuki_shogi@outlook.jp>
2025-08-06 03:29:51 +0900

    bench: 3631775

--- src/history.h
+++ src/history.h
@@ -83,7 +83,7 @@ namespace stoat {
     }
 
     [[nodiscard]] constexpr HistoryScore lmrBonus(i32 r) {
-        return static_cast<HistoryScore>(std::clamp(r * 150 - 200, 0, 2500));
+        return static_cast<HistoryScore>(std::clamp(r * 60 - 80, 0, 2500));
     }
 
     class HistoryTables {
