925ddd81edd20059316a6b756771742016f7b533
KazApps <kazuki_shogi@outlook.jp>
2025-10-04 22:20:41 +0900

    bench: 3721901

--- src/search.cpp
+++ src/search.cpp
@@ -465,7 +465,7 @@ namespace stoat {
                         beta = std::min(score + window, kScoreInf);
                     }
 
-                    window += window;
+                    window += window * (5 - reduction) / (5 - reduction + 1);
                 }
 
                 std::ranges::stable_sort(thread.rootMoves, [](const RootMove& a, const RootMove& b) {
