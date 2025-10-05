743d1f6144391cdff15c5d4a1f05a0d4c59389d8
KazApps <kazuki_shogi@outlook.jp>
2025-10-05 23:48:29 +0900

    bench: 3431067

--- src/search.cpp
+++ src/search.cpp
@@ -131,7 +131,7 @@ namespace stoat {
         }
 
         [[nodiscard]] f64 corrhistApplyComplexityFactor(i32 complexity) {
-            return complexityFactor(complexity, 1.5, 0.002);
+            return complexityFactor(complexity, 1.25, 0.005);
         }
     } // namespace
 
