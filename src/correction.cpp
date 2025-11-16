4068fa8e935f8a9c00c9da00e8037b6d20071ad5
KazApps <kazuki_shogi@outlook.jp>
2025-11-16 11:26:17 +0900

    bench: 4082105

--- src/correction.cpp
+++ src/correction.cpp
@@ -29,12 +29,15 @@ namespace stoat {
         const Position& pos,
         i32 depth,
         Score searchScore,
-        Score staticEval
+        Score staticEval,
+        i32 complexity
     ) {
         auto& tables = m_tables[pos.stm().idx()];
 
+        const double factor = 1.0 + std::log2(complexity + 1) / 10.0;
+
         const auto bonus = std::clamp(
-            static_cast<i32>((searchScore - staticEval) * depth / 8),
+            static_cast<i32>((searchScore - staticEval) * depth / 8 * factor),
             -kMaxBonus,
             kMaxBonus
         );
