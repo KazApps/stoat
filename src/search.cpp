45c79c63d8b42e630d520892fed3b85de4eee11c
KazApps <kazuki_shogi@outlook.jp>
2026-01-13 13:39:20 +0900

    bench: 6329537

--- src/search.cpp
+++ src/search.cpp
@@ -801,7 +801,7 @@ namespace stoat {
                 r += !improving;
                 r -= history / 8192;
                 r += expectedCutnode * 3;
-                r -= (40 - newPos.occupancy().popcount()) / 16;
+                r -= (40 - newPos.occupancy().popcount()) / 8;
 
                 if (pos.isInCheck()) {
                     r -= 1 + (dist == 1);
