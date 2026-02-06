9d23e1e06debb3e2a6ab3d0c86550087d0417a29
KazApps <kazuki_shogi@outlook.jp>
2026-02-06 09:59:57 +0900

    bench: 3896510

--- src/correction.h
+++ src/correction.h
@@ -46,6 +46,9 @@ namespace stoat {
             i16 value{};
 
             inline void update(i32 bonus) {
+                const auto diff = std::abs(value / 16 - bonus);
+                const auto factor = 1.0 + std::log1p(diff) / 16.0;
+                bonus = static_cast<i32>(bonus * factor);
                 value += bonus - value * std::abs(bonus) / kLimit;
             }
 
