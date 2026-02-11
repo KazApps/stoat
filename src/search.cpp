2d66451d6d589ee10a67396b597e4759c7ce9113
Ciekce <44617491+Ciekce@users.noreply.github.com>
2026-02-11 07:17:51 +0000

    double extensions (#197)
    Bench: 5797078

--- src/search.cpp
+++ src/search.cpp
@@ -779,7 +779,11 @@ namespace stoat {
                     curr.excluded = kNullMove;
 
                     if (score < sBeta) {
-                        extension = 1;
+                        if (!kPvNode && !pos.isCapture(move) && score < sBeta - 50) {
+                            extension = 2;
+                        } else {
+                            extension = 1;
+                        }
                     } else if (sBeta >= beta) {
                         return sBeta;
                     } else if (ttEntry.score >= beta) {
