6c5cf90148ee20d43c84c61207aa7121bd861a8b
KazApps <kazuki_shogi@outlook.jp>
2026-02-23 14:26:53 +0900

    bench: 9804888

--- src/search.cpp
+++ src/search.cpp
@@ -955,7 +955,7 @@ namespace stoat {
         }
 
         if (!curr.excluded) {
-            if (!pos.isInCheck() && (bestMove.isNull() || !pos.isCapture(bestMove))
+            if (!pos.isInCheck() && (bestMove.isNull())
                 && (ttFlag == tt::Flag::kExact                                          //
                     || (ttFlag == tt::Flag::kUpperBound && bestScore < curr.staticEval) //
                     || (ttFlag == tt::Flag::kLowerBound && bestScore > curr.staticEval)))
