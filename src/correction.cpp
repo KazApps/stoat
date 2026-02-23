056c834218bd4c929ca38f45114d653a5a4a0287
KazApps <kazuki_shogi@outlook.jp>
2026-02-23 14:52:56 +0900

    bench: 5157429

--- src/correction.cpp
+++ src/correction.cpp
@@ -38,7 +38,7 @@ namespace stoat {
         const double factor = 1.0 + std::log2(complexity + 1) / 10.0;
 
         const auto bonus =
-            std::clamp(static_cast<i32>((searchScore - staticEval) * depth / 8 * factor), -kMaxBonus, kMaxBonus);
+            std::clamp(static_cast<i32>((searchScore - staticEval) * depth / 4 * factor), -kMaxBonus, kMaxBonus);
 
         tables.castle[pos.castleKey() % kEntries].update(bonus);
         tables.cavalry[pos.cavalryKey() % kEntries].update(bonus);
