057b3c9995b8c95522bbb5e274d1bd97e27deb36
KazApps <kazuki_shogi@outlook.jp>
2026-03-25 08:36:52 +0900

    bench: 3425026

--- src/search.cpp
+++ src/search.cpp
@@ -753,7 +753,7 @@ namespace stoat {
                 if (depth >= 6 && ttEntry.depth >= depth - 3 && ttEntry.flag != tt::Flag::kUpperBound
                     && !isDecisive(ttEntry.score))
                 {
-                    const auto sBeta = ttEntry.score - depth * (64 - 16 * pos.isCapture(move)) / 48;
+                    const auto sBeta = ttEntry.score - depth * (64 + 16 * pos.isCapture(move)) / 48;
                     const auto sDepth = (depth - 1) / 2;
 
                     curr.excluded = move;
